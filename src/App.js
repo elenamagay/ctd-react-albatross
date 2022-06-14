b714da98f4a7b6cf6854f71586aec2aabb67dbec
Elena Magay <elenamagay1985@gmail.com>
2022-06-14 09:53:44 -0400

    react app created

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+let todoList = [
+  {
+    id: 1,
+    title: 'clean the house',
+  },
+  {
+    id: 2,
+    title: 'do homework',
+  },
+  {
+    id: 3,
+    title: 'call mom',
+  },
+  {
+    id: 4,
+    title: 'buy groceries',
+  },
+  {
+    id: 5,
+    title: 'planning a birthday party',
+  },
+];
+
+
+function App() {
+  return (
+    <div>
+      
+      <h1>Todo List</h1>
+
+      <ul>
+        {todoList.map(response => {
+          return (
+          <li key={response.id}>{response.title}</li>
+          );
+        })}
+      </ul>
+
+    </div>
+  );
+}
+
+export default App;
