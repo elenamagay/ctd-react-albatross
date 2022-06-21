697834a8f145650c3bdd9463efdb469ab8819826
Elena Magay <elenamagay1985@gmail.com>
2022-06-21 14:57:29 -0400

    a new component AddTodoForm was created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import TodoList from './TodoList';
+import AddTodoForm from './AddTodoForm';
 
 function App() {
   return (
     <div>      
       <h1>Todo List</h1>
+      <AddTodoForm />
       <TodoList />
     </div>
   );
